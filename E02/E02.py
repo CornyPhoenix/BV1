c9550fea3eb5bcd5b5e2376ea2bb371f9bc32c35
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2015-10-22 16:02:32 +0200

    Reformatting E02.

--- E02/E02.py
+++ E02/E02.py
@@ -17,104 +17,132 @@ Möllers, Konstantin (6313136)
 """
 
 from __future__ import division
-import numpy as np
-from scipy import ndimage
-from scipy import misc
-import matplotlib.pyplot as plt
 import colorsys
 
-"""
-1. PHOTOMETRY & TV IMAGING
-"""
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
 
 
-"""
-2. COLOR PERCEPTION
-"""
-
-#for y in range(len(data)):
-#    for x in range(len(data[y])):
-#        if y > 50 and y < 150:
-#            data[y][x] = [255, 125, 125]
-#        elif x > 50 and x < 150:
-#            data[y][x] = [255, 125, 0]
-#        else:
-#            data[y][x] = [0, 0, 0]
-            
 def mandelbrot(width, height, bg_color, fg_color):
     """
     Creates a Mandelbrot set with a specified width and height.
     The background and the foreground color can be given.
     """
     image = np.zeros((height, width, 3), dtype=np.uint8)
-    m = width/2
-    n = height/2
+    m = width / 2
+    n = height / 2
     for y in range(height):
         y2 = (y - n) / height * -2
         for x in range(width):
             x2 = (x - m) / height * 2 - 0.5
-            
+
             ok = True
             image[y][x] = bg_color
-            
+
             zx = 0
             zy = 0
-            
+
             xx = zx * zx
             yy = zy * zy
             xy = zx * zy
-            
-            for c in range(19):            
+
+            for c in range(19):
                 zx = xx - yy + x2
                 zy = xy + xy + y2
-                
+
                 xx = zx * zx
                 yy = zy * zy
                 xy = zx * zy
-                
+
                 if xx + yy > 4:
                     ok = False
                     break
-                    
+
             if ok:
                 image[y][x] = fg_color
-            
+
     return image
-            
+
+
 def rgb_to_hls(rgb):
     return colorsys.rgb_to_hls(rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
-    
+
+
 def brightness(rgb):
-    #return (min(rgb) + max(rgb)) / 2
+    # return (min(rgb) + max(rgb)) / 2
     return rgb_to_hls(rgb)[1] * 255
-            
+
+
 def print_brightness(image):
     target = image.copy()
     for y in range(len(image)):
         for x in range(len(image[y])):
-            rgb = image[y][x]
-            target[y][x] = brightness(rgb)
-            
+            rgb = image[y, x]
+            target[y, x] = brightness(rgb)
+
     return target
-            
+
+
 def calibrate_image(image):
-    tl = 0.5 # target brightness
+    tl = 0.5  # target brightness
     for y in range(len(image)):
         for x in range(len(image[y])):
-            h, l, s = rgb_to_hls(image[y][x])
-            
+            h, l, s = rgb_to_hls(image[y, x])
+
             r, g, b = colorsys.hls_to_rgb(h, tl, s)
             image[y][x] = [r * 255, g * 255, b * 255]
-                
-    return image
-            
-    
-data = mandelbrot(600, 400, [255, 125, 125], [0, 255, 0])
-# plt.imshow(data)
-misc.imsave("B1.png", data)
-misc.imsave("B1_Brightness.png", print_brightness(data))
 
-calibrate_image(data)
-misc.imsave("B2.png", data)
-misc.imsave("B2_Brightness.png", print_brightness(data))
-# plt.imshow(print_brightness(calibrate_image(data)))
+    return image
+
+
+class E02:
+    def __init__(self):
+        self.b1 = None
+        self.b2 = None
+
+    """
+    1. PHOTOMETRY & TV IMAGING
+    """
+
+    def exercise1a(self):
+        pass
+
+    def exercise1b(self):
+        pass
+
+    """
+    2. COLOR PERCEPTION
+    """
+
+    def exercise2a(self):
+        """
+        Generate a color image B1 with non-calibrated colors (i.e. arbitrary colors with different brightness values).
+        Determine the brightness values of background and color areas.
+        """
+        self.b1 = mandelbrot(600, 400, [255, 125, 125], [0, 255, 0])
+        plt.axis('off')
+        plt.imshow(self.b1)
+        plt.show()
+        misc.imsave("B1.png", self.b1)
+        misc.imsave("B1_Brightness.png", print_brightness(self.b1))
+
+    def exercise2b(self):
+        """
+        Using B1 as input, generate a calibrated image B2 with equally bright background and color areas.
+        """
+        self.b2 = calibrate_image(self.b1)
+        plt.axis('off')
+        plt.imshow(self.b2)
+        plt.show()
+        misc.imsave("B2.png", self.b2)
+        misc.imsave("B2_Brightness.png", print_brightness(self.b2))
+
+if __name__ == '__main__':
+    e02 = E02()
+
+    e02.exercise1a()
+    e02.exercise1b()
+    e02.exercise2a()
+    e02.exercise2b()
+
