a721a529067bbf06312048ea20aabaf3d70eea41
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2015-11-12 01:00:25 +0100

    Changed some texts, added Tim.

--- E03/1kmoelle/E03.py
+++ E03/1kmoelle/E03.py
@@ -4,15 +4,13 @@ Module:
 =======
 Bildverarbeitung 1
 
-Exercise: 
+Exercise:
 =========
 E03 - Image formation
 
 Authors:
 ========
-Toprak,  Sibel      (6712316)
-Elfaramawy, Nourhan (6517858)
-Brand,   Axel       (6145101)
+Dobert, Tim         (6)
 Möllers, Konstantin (6313136)
 
 """
@@ -37,7 +35,7 @@ def buildNickMatrix(angle):
         [0,  cos, sin],
         [0, -sin, cos]
     ]
-    
+
 def buildPanMatrix(angle):
     sin, cos = sincos(angle)
     return [
@@ -47,31 +45,31 @@ def buildPanMatrix(angle):
     ]
 
 class Vector:
-    
+
     def __init__(self, ax, ay, az):
         self.ary = [ax, ay, az]
-        
+
     def __str__(self):
         return "X = %.2fcm; Y = %.2fcm; Z = %.2fcm" % (self.x(), self.y(), self.z())
-    
+
     def x(self):
         """
         Returns the X component of this vector.
         """
         return self.ary[0]
-        
+
     def y(self):
         """
         Returns the Y component of this vector.
         """
         return self.ary[1]
-        
+
     def z(self):
         """
         Returns the Z component of this vector.
         """
         return self.ary[2]
-        
+
     def project(self, focal_length):
         """
         Projects this vector on a plane with a given focal length.
@@ -81,23 +79,23 @@ class Vector:
             self.y() * focal_length / self.z(),
             focal_length
         ]
-        
+
         return self
-    
+
     def translate(self, vec):
         """
         Translates this vector by another.
         """
         self.ary = np.add(self.ary, vec.ary)
         return self
-        
+
     def displace(self, vec):
         """
         Displaces a vector by another vector.
         """
         self.ary = np.subtract(self.ary, vec.ary)
         return self
-        
+
     def nick(self, angle):
         """
         Nicks this vector.
@@ -105,7 +103,7 @@ class Vector:
         matrix = buildNickMatrix(angle)
         self.mul(matrix)
         return self
-        
+
     def pan(self, angle):
         """
         Pans this vector.
@@ -113,23 +111,23 @@ class Vector:
         matrix = buildPanMatrix(angle)
         self.mul(matrix)
         return self
-        
+
     def mul(self, matrix):
         """
         Multiplies the vector with a matrix and sets the
         resulting coordinates on this vector.
         """
         self.ary = np.dot(matrix, self.ary)
-        
+
 
 # All lengths are given in [cm]!
-if __name__ == '__main__':    
-    
+if __name__ == '__main__':
+
     # Camera world position
     camera = Vector(0, 300, 0)
 
     # Table corner world positon
     corner = Vector(100, 75, 150)
-    
+
     # X = -0.44cm; Y = 0.50cm; Z = 3.50cm
     print corner.displace(camera).pan(45).nick(60).project(3.5)
