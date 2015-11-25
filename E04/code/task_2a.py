b8dc2ca6fbaef5c429f466fe97fc541339249581
sbltprk <sibel.toprak@outlook.com>
2015-11-25 17:51:53 +0100

    modified: task 2a (completed)

--- E04/code/task_2a.py
+++ E04/code/task_2a.py
@@ -31,23 +31,37 @@ Performing greyscale transformation on input image
 '''
 def transform_greyscale(image):
     '''Computing pixel value above darkest and below brightest percentile'''
-    percentile_dark = numpy.percentile(image, 5)
-    percentile_bright = numpy.percentile(image, 90)
-    print "Pixel value > 5th percentile:  ", percentile_dark
-    print "Pixel value < 90th percentile: ", percentile_bright
+    old_min = image.min()
+    old_max = image.max()
+    new_min = numpy.percentile(image, 5)
+    new_max = numpy.percentile(image, 90)
+    
+    print "Minimum pixel value:           ", old_min
+    print "Maximum pixel value:           ", old_max
+    print "Pixel value > 5th percentile:  ", new_min
+    print "Pixel value < 90th percentile: ", new_max
     
     '''Applying greyscale transformation to image'''
     (rows, cols) = image.shape
     
+    scaling_factor = 255 / (new_max - new_min)
+    
     for i in range(rows):
         for j in range(cols):
-            
-            if image[i,j] <= percentile_dark:
-                image[i,j] = 0 
-                
-            if image[i,j] >= percentile_bright:
-                image[i,j] = 255
-       
+            new_value_1 = image[i, j] - new_min
+            if new_value_1 >= 0:
+                image[i, j] = new_value_1
+            else:
+                image[i, j] = 0
+            new_value_2 = image[i, j] * scaling_factor
+            if new_value_2 <= 255:
+                image[i, j] = new_value_2
+            else:
+                image[i, j] = 255
+    
+    print "min: ", image.min()
+    print "max: ", image.max()
+    print image
     return image         
  
 if __name__ == "__main__":
@@ -57,7 +71,8 @@ if __name__ == "__main__":
         original = misc.imread(sys.argv[1], flatten=True)
     else:
         original = misc.lena()
-        
+    
+    
     show(original)
     #misc.imsave('original.png', original)
     
