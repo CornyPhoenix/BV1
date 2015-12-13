3f7fcd11079fe15d7ec8948885eb772c7dc6fbc4
sbltprk <sibel.toprak@outlook.com>
2015-12-13 10:37:52 +0100

    modified: - deleted some unnecessary lines from the code - rephrased some lines in 2.1b) and corrected typos in 1.2

--- E05/code/task_2_1.py
+++ E05/code/task_2_1.py
@@ -44,26 +44,3 @@ print "eigvecs ="
 print sorted_eigenvectors
 print "A_3 ="
 print A_3
-
-'''
-import numpy as np
-from numpy import linalg as la
-
-x = np.matrix([[15,5,9,3],[5,15,3,9],[9,3,15,5],[3,9,5,15]])/4
-
-ew, ev = la.eig(x)
-
-y = np.dot(ev[0:3],x)
-
-x2 = np.dot(ev[0:3].T,y)
-
-print x 
-print y
-print x2
-'''
-
-# (-> Task 2.1 b)
-
-#print A_3.T*A_3
-# Take the first line:
-# [0.75 0.25 0.25 -0.25]
